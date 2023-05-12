bf6ecf312992450ee9b96b73f9e38fdf3e79f62b
‘Ahmed <mail.ahmedwasim@gmail.com>
2023-05-12 20:18:50 +0500

    Initial Commit Installed Usefull Packages Configured Eslint Configured Tailwind

---
+++ pages/_app.jsx
@@ -0,0 +1,17 @@
+import Router from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
+
+import '../styles/globals.css'
+
+const progress = new ProgressBar({
+  size:4,
+  color:'#FE595E',
+  className:'z-50',
+  delay:100,
+})
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
+
+export default ({ Component, pageProps }) => <Component {...pageProps} />
